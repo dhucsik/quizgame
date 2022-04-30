c37a705f88f5fbb270b94506ac1e7d1f7438b6da
dias <diasgalikhanov@gmail.com>
2022-04-30 21:59:41 +0600

    first part of project

--- main.go
+++ main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main() {
+	csvFileName := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	flag.Parse()
+
+	file, err := os.Open(*csvFileName)
+	if err != nil {
+		fmt.Printf("Failed to open the CSV file: %s", *csvFileName)
+		os.Exit(1)
+	}
+
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		fmt.Println("Failed to parse the provided CSV file.")
+		os.Exit(1)
+	}
+	problems := parseLines(lines)
+
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.question)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.answer {
+			correct++
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+}
+
+type problem struct {
+	question string
+	answer   string
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			question: line[0],
+			answer:   strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
 }
